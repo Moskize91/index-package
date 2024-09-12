0422ed732a6c228f33f781ed5d5411ecd4cf5e00
TaoZeyu <i@taozeyu.com>
2024-09-12 09:42:05 +0800

    chore(project): should exit 1 when test failed

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import unittest
 
-loader = unittest.TestLoader()
-suite = loader.discover("tests")
-# suite = loader.discover("tests", pattern="test_index.py")
-runner = unittest.TextTestRunner()
-result = runner.run(suite)
+try:
+  loader = unittest.TestLoader()
+  suite = loader.discover("tests")
+  # suite = loader.discover("tests", pattern="test_index.py")
+  runner = unittest.TextTestRunner()
+  result = runner.run(suite)
+  if not result.wasSuccessful():
+    exit(1)
+except Exception as e:
+  print(e)
+  exit(1)
