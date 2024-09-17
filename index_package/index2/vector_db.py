8674db2f7f4ab876122e8a5aafc1ffe799ad5970
Moskize91 <i@taozeyu.com>
2024-09-17 12:02:33 +0800

    refactor(index): migrate and add new index logic (#8)

--- index_package/index2/vector_db.py
+++ index_package/index2/vector_db.py
@@ -1,3 +1,5 @@
+import re
+
 from typing import cast, Any, Optional
 from numpy import ndarray
 from sentence_transformers import SentenceTransformer
@@ -33,7 +35,10 @@ class VectorDB:
     node2segments: dict[str, list[tuple[float, int, int, dict]]] = {}
 
     for i in range(len(ids)):
-      node_id, _ = ids[i].split("/", 1)
+      matches = re.match(r"(.*)/([^/]*)$", ids[i])
+      if matches is None:
+        raise ValueError(f"Invalid ID: {ids[i]}")
+      node_id = matches.group(1)
       metadata: dict[str, Any] = metadatas[i]
       distance = distances[i]
       start = metadata.pop("seg_start")
@@ -67,9 +72,6 @@ class VectorDB:
     return nodes
 
   def save(self, node_id: str, segments: list[Segment], metadata: dict):
-    if len(segments) < 0:
-      return
-
     ids: list[ID] = []
     documents: list[Document] = []
     metadatas: list[Metadata] = []
