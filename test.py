46c7dfe8e1b5e34052922acdec13ca6284bd4d6d
TaoZeyu <i@taozeyu.com>
2024-09-19 10:08:53 +0800

    refactor(index): use multi threads for index event handler

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import unittest
 
 try:
   loader = unittest.TestLoader()
-  # suite = loader.discover("tests")
-  suite = loader.discover("tests", pattern="test_tasks_pool.py")
+  suite = loader.discover("tests")
+  # suite = loader.discover("tests", pattern="test_tasks_pool.py")
   runner = unittest.TextTestRunner()
   result = runner.run(suite)
   if not result.wasSuccessful():
