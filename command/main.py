b0fee8ef7831f0bcd9ffdc42ee86ab51e72c5131
TaoZeyu <i@taozeyu.com>
2024-09-21 17:06:50 +0800

    fix(command): ctrl+C not working while service is running

--- command/main.py
+++ command/main.py
@@ -7,7 +7,6 @@ def main():
   args = Args()
   command, package_path = args.parse_args()
   app = App(package_path)
-  app.signal_handler.start_watch()
 
   if isinstance(command, CommandStart):
     is_interrupted = _start_service_loop(args, app)
@@ -21,40 +20,29 @@ def main():
 
 def _start_service_loop(args: Args, app: App) -> bool:
   print("Please press your commands.\n")
-  is_last_command_is_interrupt: bool = False
   is_interrupted: bool = False
 
   while True:
-    try:
-      app.signal_handler.stop_watch()
-      command_line = input("> ")
-      app.signal_handler.start_watch()
+    app.signal_handler.mark_start_input()
+    command_line: str = input("> ")
+    print("")
+    app.signal_handler.mark_complete_input()
+    command, package_path = args.parse_args(command_line)
 
-      print("")
-      is_last_command_is_interrupt = False
-      command, package_path = args.parse_args(command_line)
+    if app.package_path != package_path:
+      print("warn: cannot change -p or --package while service is running")
 
-      if app.package_path != package_path:
-        print("warn: cannot change -p or --package while service is running")
+    if isinstance(command, CommandStart) or \
+        isinstance(command, CommandPurge):
 
-      if isinstance(command, CommandStart) or \
-         isinstance(command, CommandPurge):
+      print(f"warn: cannot run \"{command_line}\" while service is running")
+      continue
 
-        print(f"warn: cannot run \"{command_line}\" while service is running")
-        continue
+    is_interrupted = app.run(command)
+    app.signal_handler.clean_state()
 
-      is_interrupted = app.run(command)
-      if is_interrupted:
-        break
-
-    except KeyboardInterrupt:
-      if is_last_command_is_interrupt:
-        print("\nExiting...")
-        sys.exit(130)
-      else:
-        is_last_command_is_interrupt = True
-        app.signal_handler.start_watch()
-        print("\nPress Ctrl+C again to exit")
+    if is_interrupted:
+      break
 
   return is_interrupted
 
--- command/signal_handler.py
+++ command/signal_handler.py
@@ -11,13 +11,13 @@ class SignalHandler:
     self._service: Service = service
     self._scan_job: Optional[ServiceScanJob] = None
     self._first_interrupted_at: Optional[float] = None
+    self._is_inputting: bool = False
     self._lock: threading.Lock = threading.Lock()
-
-  def start_watch(self):
     signal.signal(signal.SIGINT, self._on_sigint)
 
-  def stop_watch(self):
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
+  def clean_state(self):
+    with self._lock:
+      self._first_interrupted_at = None
 
   def bind_scan_job(self, scan_job: ServiceScanJob):
     with self._lock:
@@ -29,27 +29,42 @@ class SignalHandler:
     with self._lock:
       self._scan_job = None
 
-  def _on_sigint(self, sig, frame):
-    limit_seconds = 12.0
+  def mark_start_input(self):
     with self._lock:
+      self._is_inputting = True
 
-      if self._scan_job is not None and \
-         self._first_interrupted_at is None:
-        print("\nInterrupting...")
-        self._first_interrupted_at = time.time()
-        self._scan_job.interrupt()
+  def mark_complete_input(self):
+    with self._lock:
+      self._is_inputting = False
 
-      elif self._first_interrupted_at is None:
-        print(f"\nCannot Interrupt this command (or press again to force stop after {limit_seconds}s)")
-        self._first_interrupted_at = time.time()
-
-      else:
-        duration_seconds = time.time() - self._first_interrupted_at
-        if duration_seconds <= limit_seconds:
-          str_seconds = "{:.2f}".format(limit_seconds - duration_seconds)
-          print(f"\nForce stopping... (press again to force stop after {str_seconds}s)")
+  def _on_sigint(self, sig, frame):
+    with self._lock:
+      if self._is_inputting:
+        if self._first_interrupted_at is None:
+          print("\nPress Ctrl+C again to exit")
+          self._first_interrupted_at = time.time()
         else:
-          print("\nForce stopping...")
-          print("It may corrupt the data structure of the database")
-          self._service.freeze_database()
-          sys.exit(1)
+          print("\nExiting...")
+          sys.exit(130)
+      else:
+        limit_seconds = 12.0
+        if self._scan_job is not None and \
+          self._first_interrupted_at is None:
+          print("\nInterrupting...")
+          self._first_interrupted_at = time.time()
+          self._scan_job.interrupt()
+
+        elif self._first_interrupted_at is None:
+          print(f"\nCannot Interrupt this command (or press again to force stop after {limit_seconds}s)")
+          self._first_interrupted_at = time.time()
+
+        else:
+          duration_seconds = time.time() - self._first_interrupted_at
+          if duration_seconds <= limit_seconds:
+            str_seconds = "{:.2f}".format(limit_seconds - duration_seconds)
+            print(f"\nForce stopping... (press again to force stop after {str_seconds}s)")
+          else:
+            print("\nForce stopping...")
+            print("It may corrupt the data structure of the database")
+            self._service.freeze_database()
+            sys.exit(1)
