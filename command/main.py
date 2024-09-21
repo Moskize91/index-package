a116730fed4f161cc01c521f5eef95b996e03792
Moskize91 <i@taozeyu.com>
2024-09-21 17:46:04 +0800

    feat(command): add service mode (#18)

--- command/main.py
+++ command/main.py
@@ -1,180 +1,50 @@
-import os
 import sys
-import json
-import shutil
-import yaml
-import argparse
 
-from dataclasses import dataclass
-from typing import Any, Optional
-from tqdm import tqdm
-from index_package import Service, ProgressListeners
-from .display import show_items
-from .signal_handler import SignalHandler
+from .app import App
+from .args import Args, CommandPurge, CommandStart
 
 def main():
-  parser = argparse.ArgumentParser(
-    prog="Index Package",
-    description="scan your files & save into index database",
-  )
-  parser.add_argument("text", nargs='*', type=str, default="")
-  parser.add_argument(
-    "-p", "--package",
-    default=".",
-    help="workspace directory path (default: current directory)",
-    required=False,
-    type=str,
-  )
-  parser.add_argument(
-    "--scan",
-    default=False,
-    help="scan all directories",
-    required=False,
-    type=bool,
-    action=argparse.BooleanOptionalAction,
-  )
-  parser.add_argument(
-    "--purge",
-    default=False,
-    help="purge all index data",
-    required=False,
-    type=bool,
-    action=argparse.BooleanOptionalAction,
-  )
-  parser.add_argument(
-    "--limit",
-    default=None,
-    help="workspace directory path (default: current directory)",
-    required=False,
-    type=int,
-  )
-  args = parser.parse_args()
-  package, workspace_path = _package_and_path(args.package)
-  workspace_path = os.path.join(workspace_path, "workspace")
-  exit_code: int = 0
+  args = Args()
+  command, package_path = args.parse_args()
+  app = App(package_path)
 
-  if args.purge == True:
-    if args.scan == True:
-      raise Exception("Cannot scan and purge at the same time")
-    if os.path.exists(workspace_path):
-      shutil.rmtree(workspace_path)
+  if isinstance(command, CommandStart):
+    is_interrupted = _start_service_loop(args, app)
   else:
-    embedding: str = package["embedding"]
-    sources: Optional[dict] = package["sources"]
-    if sources is None:
-      sources = {}
+    is_interrupted = app.run(command)
 
-    service = Service(
-      workspace_path=workspace_path,
-      embedding_model_id=embedding,
-    )
-    signal_handler = SignalHandler(service)
-
-    if args.scan == True:
-      listeners = _create_progress_listeners()
-      scan_job = service.scan_job(progress_listeners=listeners)
-      signal_handler.watch(scan_job)
-      success = scan_job.start(sources)
-
-      if not success:
-        print("\nComplete Interrupted.")
-        exit_code = 130
-
-      signal_handler.stop_watch()
-
-    else:
-      text = " ".join(args.text)
-      if len(text) == 0:
-        print("Text not provided")
-      else:
-        if text is None:
-          raise Exception("You can search by providing text")
-        query_result = service.query(
-          text=text,
-          results_limit=args.limit,
-        )
-        show_items(query_result)
-
-  sys.exit(exit_code)
-
-def _package_and_path(package_path: str) -> tuple[dict, str]:
-  package_path = os.path.join(os.getcwd(), package_path)
-  package_path = os.path.abspath(package_path)
-
-  if not os.path.exists(package_path):
-    raise Exception(f"Path {package_path} not found")
-
-  if os.path.isdir(package_path):
-    did_found = False
-    for ext_name in ("json", "yaml", "yml"):
-      file_path = os.path.join(package_path, f"package.{ext_name}")
-      if os.path.exists(file_path):
-        package_path = file_path
-        did_found = True
-        break
-
-    if not did_found:
-      raise Exception(f"package.json not found in {package_path}")
-
-  _, ext_name = os.path.splitext(package_path)
-
-  if ext_name == ".json":
-    with open(package_path, "r") as file:
-      package: dict = json.load(file)
-  elif ext_name == ".yaml" or ext_name == ".yml":
-    with open(package_path, "r") as file:
-      package: dict = yaml.safe_load(file)
+  if is_interrupted:
+    sys.exit(130)
   else:
-    raise Exception(f"Invalid file type {ext_name}")
+    sys.exit(1)
 
-  return package, os.path.dirname(package_path)
+def _start_service_loop(args: Args, app: App) -> bool:
+  print("Please press your commands.\n")
+  is_interrupted: bool = False
 
-@dataclass
-class _ProgressContext:
-  count: int = 0
-  files_count: int = 0
-  progress_bar: Any = None
+  while True:
+    app.signal_handler.mark_start_input()
+    command_line: str = input("> ")
+    print("")
+    app.signal_handler.mark_complete_input()
+    command, package_path = args.parse_args(command_line)
 
-def _create_progress_listeners() -> ProgressListeners:
-  context = _ProgressContext()
+    if app.package_path != package_path:
+      print("warn: cannot change -p or --package while service is running")
 
-  def on_start_scan(count: int):
-    print(f"Scanning {count} Files...")
-    context.count = count
+    if isinstance(command, CommandStart) or \
+        isinstance(command, CommandPurge):
 
-  def on_start_handle_file(path: str):
-    print(f"[{context.files_count + 1}/{context.count}] Handling File {path}")
+      print(f"warn: cannot run \"{command_line}\" while service is running")
+      continue
 
-  def on_complete_handle_file(_: str):
-    context.files_count += 1
-    close_progress_if_exists()
+    is_interrupted = app.run(command)
+    app.signal_handler.clean_state()
 
-  def on_complete_handle_pdf_page(page_index: int, total_pages: int):
-    if context.progress_bar is None:
-      context.progress_bar = tqdm(total=total_pages, desc=f"Parse PDF: {total_pages} pages", position=1)
-    context.progress_bar.update(1)
-    if page_index == total_pages - 1:
-      close_progress_if_exists()
+    if is_interrupted:
+      break
 
-  def on_complete_index_pdf_page(page_index: int, total_pages: int):
-    if context.progress_bar is None:
-      context.progress_bar = tqdm(total=total_pages, desc=f"Index PDF {total_pages}: pages", position=1)
-    context.progress_bar.update(1)
-    if page_index == total_pages - 1:
-      close_progress_if_exists()
-
-  def close_progress_if_exists():
-    if context.progress_bar is not None:
-      context.progress_bar.close()
-      context.progress_bar = None
-
-  return ProgressListeners(
-    on_start_scan=on_start_scan,
-    on_start_handle_file=on_start_handle_file,
-    on_complete_handle_pdf_page=on_complete_handle_pdf_page,
-    on_complete_index_pdf_page=on_complete_index_pdf_page,
-    on_complete_handle_file=on_complete_handle_file,
-  )
+  return is_interrupted
 
 if __name__ == "__main__":
   main()
--- command/signal_handler.py
+++ command/signal_handler.py
@@ -1,6 +1,7 @@
 import signal
 import time
 import sys
+import threading
 
 from typing import Optional
 from index_package import Service, ServiceScanJob
@@ -10,34 +11,60 @@ class SignalHandler:
     self._service: Service = service
     self._scan_job: Optional[ServiceScanJob] = None
     self._first_interrupted_at: Optional[float] = None
-
-  def watch(self, scan_job: ServiceScanJob):
-    if self._scan_job is not None:
-      raise Exception("SignalHandler already watching a scan job")
-    self._scan_job = scan_job
+    self._is_inputting: bool = False
+    self._lock: threading.Lock = threading.Lock()
     signal.signal(signal.SIGINT, self._on_sigint)
 
+  def clean_state(self):
+    with self._lock:
+      self._first_interrupted_at = None
+
+  def bind_scan_job(self, scan_job: ServiceScanJob):
+    with self._lock:
+      if self._scan_job is not None:
+        raise Exception("SignalHandler already watching a scan job")
+      self._scan_job = scan_job
+
+  def unbind_scan_job(self):
+    with self._lock:
+      self._scan_job = None
+
+  def mark_start_input(self):
+    with self._lock:
+      self._is_inputting = True
+
+  def mark_complete_input(self):
+    with self._lock:
+      self._is_inputting = False
+
   def _on_sigint(self, sig, frame):
-    if self._scan_job is None:
-      return
-
-    if self._first_interrupted_at is None:
-      print("\nInterrupting...")
-      self._first_interrupted_at = time.time()
-      self._scan_job.interrupt()
-    else:
-      duration_seconds = time.time() - self._first_interrupted_at
-      limit_seconds = 5.0
-
-      if duration_seconds > limit_seconds:
-        print("\nForce stopping...")
-        print("It may corrupt the data structure of the database")
-        self._service.freeze_database()
-        sys.exit(1)
+    with self._lock:
+      if self._is_inputting:
+        if self._first_interrupted_at is None:
+          print("\nPress Ctrl+C again to exit")
+          self._first_interrupted_at = time.time()
+        else:
+          print("\nExiting...")
+          sys.exit(130)
       else:
-        str_seconds = "{:.2f}".format(limit_seconds - duration_seconds)
-        print(f"\nForce stopping... (press again to force stop after {str_seconds}s)")
+        limit_seconds = 12.0
+        if self._scan_job is not None and \
+          self._first_interrupted_at is None:
+          print("\nInterrupting...")
+          self._first_interrupted_at = time.time()
+          self._scan_job.interrupt()
 
-  def stop_watch(self):
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-    self._scan_job = None
+        elif self._first_interrupted_at is None:
+          print(f"\nCannot Interrupt this command (or press again to force stop after {limit_seconds}s)")
+          self._first_interrupted_at = time.time()
+
+        else:
+          duration_seconds = time.time() - self._first_interrupted_at
+          if duration_seconds <= limit_seconds:
+            str_seconds = "{:.2f}".format(limit_seconds - duration_seconds)
+            print(f"\nForce stopping... (press again to force stop after {str_seconds}s)")
+          else:
+            print("\nForce stopping...")
+            print("It may corrupt the data structure of the database")
+            self._service.freeze_database()
+            sys.exit(1)
