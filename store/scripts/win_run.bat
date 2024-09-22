bc960400f404db267c73ac020e9e0011364c1590
Moskize91 <i@taozeyu.com>
2024-09-22 13:26:31 +0800

    feat(scanner): support windows (#19)

---
+++ store/scripts/win_run.bat
@@ -0,0 +1,2 @@
+@echo off
+..\..\.venv\Scripts\python.exe ..\..\bin\index_for_windows.py start -p ..\package.yaml
---
+++ store/scripts/win_setup.bat
@@ -0,0 +1,16 @@
+
+where python > nul 2>&1
+if %errorlevel% neq 0 (
+    echo please install python first.
+    pause
+    exit
+)
+
+cd ..\..\
+python -m venv .venv
+.venv\Scripts\python.exe -m pip install --upgrade pip
+.venv\Scripts\pip.exe install -r requirements.txt
+.venv\Scripts\python.exe -m spacy download en_core_web_sm
+.venv\Scripts\python.exe -m spacy download zh_core_web_sm
+
+pause
